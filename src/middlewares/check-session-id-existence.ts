e019d9e825bad4db081ca9383f08c74ce37beb7a
KpSantiago <santiago.kauajose11@gmail.com>
2024-04-24 21:40:58 -0300

    test

--- src/middlewares/check-session-id-existence.ts
+++ src/middlewares/check-session-id-existence.ts
@@ -2,7 +2,6 @@ import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function checkSessionIdExistence(request: FastifyRequest, reply: FastifyReply) {
     const { sessionId } = request.cookies;
-    console.log(sessionId)
     if (!sessionId) {
         return reply.status(401).send({
             error: 'Unauthorized.'
